9f685696d161facdbb768006d72a888185101295
mari <mari.17ymy@gmail.com>
2020-06-01 01:47:58 -0400

    menu desplegable

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,15 +8,56 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('inicio'),
       ),
-      body: Column(
+      drawer: _crearDrawer(context),
+      body: 
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('color secundario'),
+            Divider(),
+            Text('genero'),
+            Divider(),
+            Text('nombre usuario'),
+          ],
+        ),
+    );
+  }
+
+  Drawer _crearDrawer(BuildContext context){
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
         children: [
-          Text('color secundario'),
-          Divider(),
-          Text('genero'),
-          Divider(),
-          Text('nombre usuario'),
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage('assets/menu-img.jpg'), fit: BoxFit.cover)
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.ac_unit, color: Colors.blue,),
+            title: Text('uno'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.ac_unit, color: Colors.blue,),
+            title: Text('dos'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.ac_unit, color: Colors.blue,),
+            title: Text('people'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings, color: Colors.blue,),
+            title: Text('Settings'),
+            onTap: (){
+              Navigator.pushReplacementNamed(context, 'setting');
+            },
+          ),
         ],
-      ),
+      ) 
     );
   }
 }
