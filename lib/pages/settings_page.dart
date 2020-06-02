78882c5b8b926525e809103debaeca214d65d9a2
mari <mari.17ymy@gmail.com>
2020-06-01 22:03:11 -0400

    pagina de ajustes

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:preferencias_de_usuario/widgets/menu_widget.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _colorSecundario = true;
+  int _genero = 1;
+  String _nombre = 'Pedro';
+
+  TextEditingController _textController;
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController(text: _nombre);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +26,49 @@ class SettingsPage extends StatelessWidget {
         title: Text('settings'),
       ),
       drawer: MenuWidget(),
-      body: Center(
-        child: Text('ajustes')
-      ),
+      body: ListView(
+        children: [
+          Text('Settings', 
+            style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold) 
+          ),
+          SwitchListTile(
+            title: Text('Color secundario'),
+            value: _colorSecundario, 
+            onChanged: (value){
+              _colorSecundario = value;
+              setState(() {  });
+            }
+          ),
+          RadioListTile(
+            title: Text('Masculino'),
+            value: 1, 
+            groupValue: _genero, 
+            onChanged: (value){
+              _genero = value;
+              setState(() {  });
+            }
+          ),
+          RadioListTile(
+            title: Text('Femenino'),
+            value: 2, 
+            groupValue: _genero, 
+            onChanged: (value){
+              _genero = value;
+              setState(() {  });
+            }
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              controller: TextEditingController(text: _nombre),
+              decoration: InputDecoration(
+                labelText: 'Nombre',
+                helperText: 'Nombre de la persona usando el telefono'
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
